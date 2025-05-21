0baf161280866276e9011b5bde72368ff57c33b7
Antoine Corrignan <166537198+AntoineCorrignan@users.noreply.github.com>
2025-05-21 12:43:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,16 +24,45 @@ authenticator = stauth.Authenticate(
 authenticator.login()
 
 
-def accueil():
-    st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
+def page_accueil():
+    st.title("Bienvenue sur la page d'accueil")
+    st.write("Ceci est la page d'accueil de votre application.")
 
 
-if st.session_state["authentication_status"]:
-    accueil()
-    # Le bouton de déconnexion
-    authenticator.logout("Déconnexion")
+def page_photos_chat():
+    st.title("Photos de mon chat")
+    st.write("Voici quelques photos de mon chat.")
+    # Ajoutez ici le code pour afficher les photos
 
-elif st.session_state["authentication_status"] is False:
-    st.error("L'username ou le password est/sont incorrect")
-elif st.session_state["authentication_status"] is None:
-    st.warning("Les champs username et mot de passe doivent être remplie")
+
+def main():
+    if st.session_state["authentication_status"]:
+        # Utilisation de st.columns pour centrer le titre
+        col1, col2, col3 = st.columns([1, 2, 1])
+        with col2:
+            st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
+
+        # Utilisation de st.sidebar pour la barre latérale
+        with st.sidebar:
+            # Bouton de déconnexion
+            authenticator.logout("Déconnexion")
+
+            # Menu d'arborescence avec streamlit-option-menu
+            selected = option_menu(
+                menu_title="Menu",  # Titre du menu
+                options=["Accueil", "Photos de mon chat"],  # Options du menu
+                icons=["house", "camera"],  # Icônes pour chaque option
+                menu_icon="cast",  # Icône du menu
+                default_index=0,  # Option sélectionnée par défaut
+            )
+
+        # Affichage de la page sélectionnée
+        if selected == "Accueil":
+            page_accueil()
+        elif selected == "Photos de mon chat":
+            page_photos_chat()
+
+    elif st.session_state["authentication_status"] is False:
+        st.error("L'username ou le password est/sont incorrect")
+    elif st.session_state["authentication_status"] is None:
+        st.warning("Les champs username et mot de passe doivent être remplis")
